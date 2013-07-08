be3973ad1c8ce7da21ceff1e413ef9d137e8978e
humanbagel <los.angeles.lakers@gmail.com>
2013-07-07 23:01:26 -0700

    oops forgot a change for socket.io

--- config/app.js
+++ config/app.js
@@ -161,7 +161,7 @@ app.listen(port);
 
 // express 3 requires instantiating a http.Server to attach socket.io to
 var server = require('http').createServer(app)
-app.ws = require('socket.io').listen(app);
+app.ws = require('socket.io').listen(server);
 app.ws.set('log level', 1);
 app.ws.set('browser client minification', true);
 
