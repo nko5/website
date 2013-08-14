120589f3d830849b056bc04dd0cf26791b2b5d28
David Dias <daviddias.p@gmail.com>
2013-08-14 22:11:53 +0100

    401 to all the sneaky ones  fix #3

--- config/app.js
+++ config/app.js
@@ -161,15 +161,16 @@ app.configure(function() {
   // });
 
   app.use(function (err, req, res, next) { 
-    if (err) {
-      return res.render2('errors/' + err.status, { status: err.status });
-    }
     if (typeof(err) === 'number'){
       return res.render2('errors/' + err, { status: err });
     }
     if (typeof(err) === 'string'){
       err = Error(err);
     }  
+    if (err) {
+      return res.render2('errors/' + err.status, { status: err.status });
+    }
+
     console.error(err.stack);
     res.render2('errors/500', { error: err });
   });
