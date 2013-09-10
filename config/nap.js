da8d04936fd8e699d16eaf2069f735088facd642
Jacques Crocker <railsjedi@gmail.com>
2013-09-10 15:02:02 -0700

    wtfshit

--- config/nap.js
+++ config/nap.js
@@ -2,6 +2,7 @@ var env = require('./env');
 var util = require('util');
 global.nap = require('nap');
 
+process.chdir(__dirname+"../");
 util.debug('NAP CURRENT DIRECTORY: ' + process.cwd());
 
 nap({
