01b4b3c5a14068d68c0fb7f4302fb5296ed7c3a1
David Dias <daviddias.p@gmail.com>
2013-10-30 20:22:25 +0000

    uncommented nko module to be ready on prod

--- module/nko.js
+++ module/nko.js
@@ -8,27 +8,24 @@ module.exports = function ping(code, callback) {
   if (typeof code !== 'string')
     throw Error('Go to http://nodeknockout.com/teams/mine to get your code.');
 
-  // var subdomain = process.env.SUBDOMAIN
-  //   , hostname = subdomain ? subdomain : os.hostname();
 
   var params = {
-    // hostname: hostname,
     os: os.type(),
     release: os.release(),
     teamcode: code,
   },
-  // options = {
-  //   host: 'nodeknockout.com',
-  //   port: 80,
-  //   path: '/deploys?' + qs.stringify(params)
-  // };
-
-  // For testing
   options = {
-    host: 'localhost',
-    port: 8003,
+    host: 'nodeknockout.com',
+    port: 80,
     path: '/deploys?' + qs.stringify(params)
   };
+
+  // For testing
+  // options = {
+  //   host: 'localhost',
+  //   port: 8003,
+  //   path: '/deploys?' + qs.stringify(params)
+  // };
 
 
   setTimeout(function (){
