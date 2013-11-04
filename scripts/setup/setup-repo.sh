9c369e75234722886a76d52d4f75750a0a576eb3
Gerad Suyderhoud <gerad@modernstack.com>
2013-11-04 15:38:24 -0800

    tweak

--- scripts/setup/setup-repo.sh
+++ scripts/setup/setup-repo.sh
@@ -132,6 +132,7 @@ var port = (isProduction ? 80 : 8000);
 http.createServer(function (req, res) {
   // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
   var voteko = '<iframe src="http://nodeknockout.com/iframe/${slug}" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
+
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<html><body>' + voteko + '</body></html>\n');
 }).listen(port, function(err) {
